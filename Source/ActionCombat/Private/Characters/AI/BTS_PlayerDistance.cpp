eb0b34dfd37b61d05f6d180342263734002fabf8
Tyler Baylson <thbaylson@gmail.com>
2025-03-03 21:29:40 -0500

    Lesson 6.31: Creating Behavior Tree Services.

---
+++ Source/ActionCombat/Private/Characters/AI/BTS_PlayerDistance.cpp
@@ -0,0 +1,13 @@
+#include "Characters/AI/BTS_PlayerDistance.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void UBTS_PlayerDistance::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	FVector CurrentLocation{ OwnerComp.GetAIOwner()->GetPawn()->GetActorLocation() };
+	FVector PlayerLocation{ GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation() };
+
+	float Distance{ static_cast<float>(FVector::Distance(CurrentLocation, PlayerLocation)) };
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsFloat("Distance", Distance);
+}
---
+++ Source/ActionCombat/Public/Characters/AI/BTS_PlayerDistance.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTS_PlayerDistance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONCOMBAT_API UBTS_PlayerDistance : public UBTService
+{
+	GENERATED_BODY()
+	
+protected:
+	void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+};
